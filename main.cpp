4566d59fb4f7b4af0c31e502a8d7a442ba1647ff
midnight <kevinrodriguez4851@gmail.com>
2023-02-07 18:18:56 -0600

    refactor: start function into lambda

--- main.cpp
+++ main.cpp
@@ -4,42 +4,41 @@
 unsigned int vertex_array;
 unsigned int vertex_buffer;
 
-void start_function()
-{
-  /* create data for vertex */
-  float data[]
-  {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f,
-  };
-
-  /* generate arrays and buffers */
-  glGenVertexArrays(1, &vertex_array);
-  glGenBuffers(1, &vertex_buffer);
-
-  /* bind buffers to store data */
-  glBindVertexArray(vertex_array);
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_array);
-
-  /* store data in buffer */
-  glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
-
-  /* specify data layout */
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
-  glEnableVertexAttribArray(0);
-
-  /* unbind */
-  glBindVertexArray(0);
-}
- 
 int main()
 {
   Window example_window;
 
   Shader shader_program("./example.shader");
 
-  example_window.start(start_function);
+  example_window.start([&]()
+  {
+    /* create data for vertex */
+    float data[]
+    {
+      -0.5f, -0.5f, 0.0f,
+       0.5f, -0.5f, 0.0f,
+       0.0f,  0.5f, 0.0f,
+    };
+
+    /* generate arrays and buffers */
+    glGenVertexArrays(1, &vertex_array);
+    glGenBuffers(1, &vertex_buffer);
+
+    /* bind buffers to store data */
+    glBindVertexArray(vertex_array);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_array);
+
+    /* store data in buffer */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+
+    /* specify data layout */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    /* unbind */
+    glBindVertexArray(0);
+
+  });
 
   example_window.update([&]()
   {
